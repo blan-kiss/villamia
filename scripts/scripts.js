9913e3383dccef795259063510de6926dc3ea398
blan-kiss <85528999+blan-kiss@users.noreply.github.com>
2021-09-20 18:33:15 -0500

    Initial setup

---
+++ scripts/scripts.js
@@ -0,0 +1,17 @@
+function changeLogo() {
+    // var num; //Declaration
+    // num = 10; // Initialization
+
+    // var num = 10; // Declaration and Initialization at the same time
+    // console.log(num);
+
+    var logoB = document.getElementById('logo');
+    logoB.src = 'imgs/logoB2.png';
+}
+
+function reloadLogo() {
+    var logoBv2 = document.getElementById('logo');
+    logoBv2.src = 'imgs/logoB1.png';
+
+}
+
