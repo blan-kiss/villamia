c523cdbf3197193c0a487b7a4efb6b12157c78be
Blanca Berenice <blancaberenice@iMac-de-Blanca-2.local>
2021-10-06 14:31:30 -0500

    feature: adding draggable option for carousel

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@ window.addEventListener('load', function(){
 	new Glider(document.querySelector('.carousel__lista'), {
 		slidesToShow: 1,
 		slidesToScroll: 1,
+		draggable: true,
 		dots: '.carousel__indicadores',		
 		arrows: {
 			prev: '.carousel__anterior',
@@ -20,8 +21,8 @@ window.addEventListener('load', function(){
 			  // screens greater than >= 1024px
 			  breakpoint: 800,
 			  settings: {
-				slidesToShow: 4,
-				slidesToScroll: 4
+				slidesToShow: 3,
+				slidesToScroll: 3
 			  }
 			}
 		]
