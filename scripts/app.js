52c70d14e5cdbccbf71e1683bfc48e84e2739046
blan-kiss <bbgr04@hotmail.com>
2022-01-12 19:15:43 -0600

    feature: new carousel_taquiza

--- scripts/app.js
+++ scripts/app.js
@@ -32,4 +32,91 @@ window.addEventListener('load', function(){
 			}
 		]
 	});
+
+
+
+
+	// Segundo carrusel--------------
+
+	new Glider(document.querySelector('.carousel__lista_gal1'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: false,
+		scrollLock: false,
+		dots: '.carousel__indicadores_gal1',		
+		arrows: {
+			prev: '.carousel__anterior_gal1',
+			next: '.carousel__siguiente_gal1'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: '1',
+				slidesToScroll: '1',
+				// draggable: true,
+				// scrollLock: false
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				
+				// draggable: true,
+				// scrollLock: false
+			  }
+			}
+		]
+	});
+
+
+
+
+
+
+	// Carrusel taquiza--------------
+
+	new Glider(document.querySelector('.carousel__lista_taco'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: false,
+		scrollLock: false,
+		dots: '.carousel__indicadores_taco',		
+		arrows: {
+			prev: '.carousel__anterior_taco',
+			next: '.carousel__siguiente_taco'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: '1',
+				slidesToScroll: '1',
+				// draggable: true,
+				// scrollLock: false
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				
+				// draggable: true,
+				// scrollLock: false
+			  }
+			}
+		]
+	});
+
 });
+
+
+
+
