f9c1b79b48ca11c7e4422b8062232a0ccf0574c0
blan-kiss <bbgr04@hotmail.com>
2022-01-19 15:32:10 -0600

    feature: new carousels  (taquiza, guisados, tamales, postres,hamburguesas y hot dogs e inflable) responsive mobile y tableta

--- scripts/app.js
+++ scripts/app.js
@@ -115,6 +115,217 @@ window.addEventListener('load', function(){
 		]
 	});
 
+
+
+
+
+	// Carrusel guisados --------------
+
+	new Glider(document.querySelector('.carousel__lista_guisados'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: false,
+		scrollLock: false,
+		dots: '.carousel__indicadores_guisados',		
+		arrows: {
+			prev: '.carousel__anterior_guisados',
+			next: '.carousel__siguiente_guisados'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: '1',
+				slidesToScroll: '1',
+				// draggable: true,
+				// scrollLock: false
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				
+				// draggable: true,
+				// scrollLock: false
+			  }
+			}
+		]
+	});
+
+
+
+
+
+	// Carrusel postres --------------
+
+	new Glider(document.querySelector('.carousel__lista_postres'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: false,
+		scrollLock: false,
+		dots: '.carousel__indicadores_postres',		
+		arrows: {
+			prev: '.carousel__anterior_postres',
+			next: '.carousel__siguiente_postres'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: '1',
+				slidesToScroll: '1',
+				// draggable: true,
+				// scrollLock: false
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				
+				// draggable: true,
+				// scrollLock: false
+			  }
+			}
+		]
+	});
+
+
+
+
+	// Carrusel hamburguesas y hotdogs --------------
+
+	new Glider(document.querySelector('.carousel__lista_hamhot'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: false,
+		scrollLock: false,
+		dots: '.carousel__indicadores_hamhot',		
+		arrows: {
+			prev: '.carousel__anterior_hamhot',
+			next: '.carousel__siguiente_hamhot'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: '1',
+				slidesToScroll: '1',
+				// draggable: true,
+				// scrollLock: false
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				
+				// draggable: true,
+				// scrollLock: false
+			  }
+			}
+		]
+	});
+
+
+
+
+
+	// Carrusel tamales --------------
+
+	new Glider(document.querySelector('.carousel__lista_tamales'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: false,
+		scrollLock: false,
+		dots: '.carousel__indicadores_tamales',		
+		arrows: {
+			prev: '.carousel__anterior_tamales',
+			next: '.carousel__siguiente_tamales'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: '1',
+				slidesToScroll: '1',
+				// draggable: true,
+				// scrollLock: false
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				
+				// draggable: true,
+				// scrollLock: false
+			  }
+			}
+		]
+	});
+
+
+
+	// Carrusel tamales --------------
+
+	new Glider(document.querySelector('.carousel__lista_inflable'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: false,
+		scrollLock: false,
+		dots: '.carousel__indicadores_inflable',		
+		arrows: {
+			prev: '.carousel__anterior_inflable',
+			next: '.carousel__siguiente_inflable'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: '1',
+				slidesToScroll: '1',
+				// draggable: true,
+				// scrollLock: false
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				
+				// draggable: true,
+				// scrollLock: false
+			  }
+			}
+		]
+	});
+
+
+
+
+
+
+
+
+
+
 });
 
 
